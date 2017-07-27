d79bc79ab6d3153c40d16e73f0bb4f49dbdf14a7
TranHung92 <tranqh161@gmail.com>
2017-07-28 00:16:52 +1000

    build server

---
+++ server/build/models/comment.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose'),
+      Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+	content: String,
+	author: {
+		id: {
+			type: Schema.Types.ObjectId,
+			ref: "User"
+		},
+		username: String
+	}
+});
+
+const commentModel = mongoose.model('comment', commentSchema);
+module.exports = commentSchema;
---
+++ server/build/router.js
@@ -0,0 +1,21 @@
+const Authentication = require('./controllers/authentication');
+const Story = require('./controllers/story');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
+
+module.exports = function (app) {
+  app.get('/', Story.showStories);
+  app.post('/signin', requireSignin, Authentication.signin);
+  app.post('/signup', Authentication.signup);
+  app.post('/new-story', Story.newStory);
+  app.get('/story/:id', Story.readStory);
+  app.get('/auth', requireAuth, function (req, res) {
+    res.send({ message: "blah blah blah..." });
+  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  });
+};
