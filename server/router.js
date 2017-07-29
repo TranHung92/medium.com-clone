49f1fe0af6799d363e423bc0e99984e388384eb0
TranHung92 <tranqh161@gmail.com>
2017-07-29 22:43:25 +1000

    fix path not define

--- server/router.js
+++ server/router.js
@@ -2,6 +2,7 @@ const Authentication = require('./controllers/authentication');
 const Story = require('./controllers/story');
 const passportService = require('./services/passport');
 const passport = require('passport');
+const path = require('path');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
