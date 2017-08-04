c13c3b6609b4d71a04c34c87cce39190d2e5f241
TranHung92 <tranqh161@gmail.com>
2017-08-04 19:10:46 +1000

    signin/signup modal

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Header, Image, Modal } from 'semantic-ui-react'
 import Signin from './auth/signin'
+import Signup from './auth/signup'
 
-const ModalAuth = ({trigger}) => (
-  <Modal size='small' dimmer='inverted' trigger={trigger}>
-    <Modal.Header>Signin/Signup</Modal.Header>
-    <Modal.Content>
-    	<Signin />
-    </Modal.Content>
-  </Modal>
-)
+class ModalAuth extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			signin: true
+		}
+	}
+
+	onClick() {
+		this.setState({ signin: !this.state.signin })
+		console.log('signin', this.state.signin)
+	}
+
+	render() {
+		console.log('state',this.state.signin)
+		return (
+		  <Modal size='small' dimmer='inverted' trigger={this.props.trigger}>
+		    <Modal.Header>
+		    	{this.state.signin ? 'Signin' : 'Signup'}
+		    </Modal.Header>
+		    <Modal.Content>
+		    	{this.state.signin ? <Signin /> : <Signup />}
+		    </Modal.Content>
+		    <div onClick={this.onClick.bind(this)}>
+		    	<h3>{this.state.signin}</h3>
+		    	<a type="button">{this.state.signin ? 'Signup' : 'Signin'}</a>
+		    </div>
+		  </Modal>
+		)
+	}
+}
+
+ 
 
 export default ModalAuth
